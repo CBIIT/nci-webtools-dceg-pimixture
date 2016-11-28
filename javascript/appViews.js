3ddac99f7c637d8342b727e9834a00ce962a09ab
Wesley Obenshain <umdshaman@gmail.com>
2016-11-28 02:02:59 -0500

    refactor for additional parameters

--- javascript/appViews.js
+++ javascript/appViews.js
@@ -19,84 +19,40 @@ appMixture.FormView = Backbone.View.extend({
         var $that = this;
         this.model.on({
             'change:csvFile': this.updateOptions,
-            'change:design': this.showNext,
-            'change:model': this.showNext,
-            'change:outcomeC': this.showNext,
-            'change:outcomeL': this.showNext,
-            'change:outcomeR': this.showNext,
-            'change:covariates': this.showNext,
-            'change:groupTrigger': this.resetGroup,
-            'change:email': this.showNext
+            'change:design': this.changeDesign,
+            'change:model': this.changeModel,
+            'change:outcomeC': this.changeCovariates,
+            'change:outcomeL': this.changeCovariates,
+            'change:outcomeR': this.changeCovariates,
+            'change:covariates': this.changeCovariates,
+            'change:email': this.changeEmail
         }, this);
         this.$el.find('[name="covariates"]').selectize({
             plugins: ['remove_button'],
             sortField: 'order'
         });
-        this.groupIndex = {};
-        var lastGroup = null;
-        this.$el.find('fieldset').each(function (index,e) {
-            var group = {
-                    'head': e,
-                    'subs': []
-                },
-                skipNext = false,
-                subs = group.subs;
-            if (lastGroup !== null) {
-                lastGroup.next = group;
-            }
-            $(e).find('select,input').each(function(index2,e2) {
-                var lastIndex = subs.length;
-                if (skipNext) {
-                    skipNext = false;
-                    return;
-                }
-                if (e2.selectize) {
-                    skipNext = true;
-                    subs.push({
-                        'element': e2,
-                        'name': e2.name,
-                        'type': 'selectize'
-                    });
-                } else if (e2.tagName == 'SELECT') {
-                    subs.push({
-                        'element': e2,
-                        'name': e2.name,
-                        'type': 'select'
-                    });
-                } else {
-                    subs.push({
-                        'element': e2,
-                        'name': e2.name,
-                        'type': e2.type
-                    });
-                }
-                subs[lastIndex].empty = $that.model.defaults[subs[lastIndex].name];
-                $that.groupIndex[e2.name] = group;
-            });
-            lastGroup = group;
-        });
     },
     events: {
         'change input[type="file"]': 'uploadFile',
         'change input.selectized': 'updateModel',
+        'change input[type="text"]': 'updateModel',
         'keyup input[type="text"]': 'updateModel',
+        'change input[type="checkbox"]': 'updateModel',
         'change select': 'updateModel',
-        'click #dataGroups': 'openDataGroups'
+        'click #effectsButton': 'openInteractiveEffects',
+        'click #referencesButton': 'openReferenceGroups'
     },
-    openDataGroups: function(e) {
+    openInteractiveEffects: function(e) {
         e.preventDefault();
-        new appMixture.DataGroupsView({
+        new appMixture.InteractiveEffectsView({
             model: this.model
         });
     },
-    updateModel: function(e) {
+    openReferenceGroups: function(e) {
         e.preventDefault();
-        var e = $(e.target),
-            name = e.prop('name'),
-            val = e.val();
-        if (val === "null") val = null;
-        if (!Number.isNaN(parseInt(val))) val = parseInt(val);
-        this.model.set(name,val);
+        new appMixture.ReferenceGroupsView({
+            model: this.model
+        });
     },
     uploadFile: function(e) {
         e.preventDefault();
@@ -125,50 +81,64 @@ appMixture.FormView = Backbone.View.extend({
             }
         }
     },
+    updateModel: function(e) {
+        e.preventDefault();
+        var e = $(e.target),
+            name = e.prop('name'),
+            val = e.val();
+        switch (e.prop('type')) {
+            case 'checkbox':
+                val = e.prop('checked') || false;
+                break;
+            default:
+                if (val === "null") val = null;
+                if (!Number.isNaN(parseInt(val))) val = parseInt(val);
+                break;
+        }
+        this.model.set(name,val);
+    },
+    changeCovariates: function() {
+        var model = this.model;
+        if (model.get('outcomeC') === "" || model.get('outcomeL') === "" || model.get('outcomeR') === "" || model.get('covariates') === "") {
+            this.clearAfter('#covariatesSet');
+        } else {
+            this.showNext('#covariatesSet');
+        }
+    },
+    changeDesign: function() {
+        if (this.model.get('design') === "") {
+            this.clearAfter('#designSet');
+        } else {
+            this.showNext('#designSet');
+        }
+    },
+    changeEmail: function() {
+        if (this.model.get('email') === "") {
+            this.clearAfter('#emailSet');
+        } else {
+            this.showNext('#emailSet');
+        }
+    },
+    changeModel: function() {
+        if (this.model.get('model') === "") {
+            this.clearAfter('#modelSet');
+        } else {
+            this.showNext('#modelSet');
+        }
+    },
     resetGroup: function() {
         this.model.set('groupValue',[]);
         this.showNext.apply(this);
     },
-    showNext: function() {
-        var $that = this,
-            attrs = this.model.changedAttributes();
-        for (var key in attrs) {
-            var group = $that.groupIndex[key],
-                show = true;
-            if (group && group.next) {
-                for (var index in group.subs) {
-                    var sub = group.subs[index];
-                    if ($that.model.get(sub.name) === sub.empty) show = false;
-                }
-                $(group.next.head).toggleClass('show',show);
-                if (!show) {
-                    group = group.next;
-                    for (var index in group.subs) {
-                        var sub = group.subs[index];
-                        switch (sub.type) {
-                            case 'selectize':
-                                sub.element.selectize.clear();
-                                break;
-                            case 'select':
-                                $(sub.element).prop('selectedIndex',0);
-                                $that.model.set(sub.name,sub.empty);
-                                break;
-                            case 'button':
-                                break;
-                            default:
-                                $(sub.element).val(sub.empty);
-                                $that.model.set(sub.name,sub.empty);
-                                break;
-                        }
-                    }
-                }
-            }
-        }
-    },
     updateOptions: function() {
-        this.showNext.apply(this);
-        var contents = this.model.get('csvFile'),
-            optionsList = "<option value=\"\">----Select Outcome----</option>",
+        var contents = this.model.get('csvFile');
+        if (contents) {
+            this.showNext('#fileSet');
+        } else {
+            this.clearAfter('#fileSet');
+            return;
+        }
+        var optionsList = "<option value=\"\">----Select Outcome----</option>",
             covariates = $('[name="covariates"]')[0].selectize;
         if (options === null) return;
         var options = Object.keys(contents[0]).map(function(e) { return {'text':e,'value':e.replace(/"/g,'&quot;')}; });
@@ -180,10 +150,132 @@ appMixture.FormView = Backbone.View.extend({
         this.$el.find('[name="outcomeR"]').html(optionsList);
         covariates.clearOptions();
         covariates.addOption(options);
+    },
+    clearAfter: function(id) {
+        console.log(id);
+        var next = $(id).next();
+        console.log(next);
+        next.find('input,select,.selectized').each(function(index,child) {
+            var $child = $(child);
+            if (child.selectize) {
+                child.selectize.clear();
+            } else {
+                switch ($child.prop('tagName')) {
+                    case 'SELECT':
+                        child.selectedIndex = 0;
+                        $child.trigger('change');
+                        break;
+                    case 'INPUT':
+                        switch ($child.prop('type')) {
+                            case 'button':
+                                return;
+                            case 'checkbox':
+                                $child.removeAttr('checked');
+                                break;
+                            default:
+                                $child.val('');
+                                break;
+                        }
+                        $child.trigger('change');
+                        break;
+                }
+            }
+        });
+        next.removeClass('show');
+    },
+    showNext: function(id) {
+        $(id).next().addClass('show');
     }
 });
 
-appMixture.DataGroupsView = Backbone.View.extend({
+
+appMixture.InteractiveEffectsView = Backbone.View.extend({
+    initialize: function() {
+        var headers = this.model.get('covariates');
+        this.model.on({
+            'change:groupValue': this.renderTable
+        }, this)
+        this.render();
+    },
+    events: {
+        'hidden.bs.modal': 'remove',
+        'keyup input[type="text"]': 'updateModel',
+        'click .remove': 'removeEntry',
+        'click .add': 'addEntry'
+    },
+    addEntry: function(e) {
+        var target = $(e.target),
+            inputs = target.parent().parent().find('input'),
+            covariates = this.model.get('covariates').split(','),
+            groupValue = this.model.get('groupValue');
+        var obj = {};
+        for (var index in covariates) {
+            var input = inputs.eq(index);
+            obj[covariates[index]] = input.val();
+            input.val('');
+        }
+        groupValue.push(obj);
+        this.model.trigger('change:groupValue',this.model);
+        this.model.set({
+            'groupTrigger': 'something'
+        });
+    },
+    removeEntry: function(e) {
+        var index = $(e.target).prop('name'),
+            groupValue = this.model.get('groupValue');
+        groupValue.splice(index,1);
+        this.model.trigger('change:groupValue',this.model);
+        if (groupValue.length < 1) {
+            this.model.set({
+                'groupTrigger': ''
+            });
+        }
+    },
+    updateModel: function(e) {
+        if (e.keyCode == 13) {
+            this.createList.call(this,e);
+        } else {
+            appMixture.events.updateModel.call(this,e);
+        }
+    },
+    render: function() {
+        var covariates = this.model.get('covariates').split(',');
+        this.$modal = BootstrapDialog.show({
+            'message': $("<table><thead></thead><tbody></tbody><tfoot></tfoot></table>"),
+            'title': "Enter Data Groups"
+        });
+        this.setElement(this.$modal.getModal());
+        var tfoot = "<tr>";
+        for (var index in covariates) {
+            tfoot += "<td><input type=\"text\" placeholder=\""+covariates[index]+"\"/></td>";
+        }
+        tfoot += "<td class=\"borderless\"><button class=\"add disabled\">Add</button></td></tr>";
+        this.$el.find('tfoot').empty().append(tfoot);
+        this.renderTable.apply(this);
+    },
+    renderTable: function() {
+        var covariates = this.model.get('covariates').split(','),
+            groupValue = this.model.get('groupValue'),
+            thead = "<tr>",
+            tbody = "",
+            tfoot = "<tr>";
+        for (var index in covariates) {
+            thead += "<th>"+covariates[index]+"</th>";
+        }
+        for (var index in groupValue) {
+            tbody += "<tr>";
+            for (var index2 in covariates) {
+                tbody += "<td>"+groupValue[index][covariates[index2]]+"</td>"
+            }
+            tbody += "<td class=\"borderless\"><button name=\""+index+"\" class=\"remove\">X</button></td></tr>";
+        }
+        thead += "<th class=\"borderless\"></th></tr>";
+        this.$el.find('thead').empty().append(thead);
+        this.$el.find('tbody').empty().append(tbody);
+    }
+});
+
+appMixture.ReferenceGroupsView = Backbone.View.extend({
     initialize: function() {
         var headers = this.model.get('covariates');
         this.model.on({
