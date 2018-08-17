7ea589517374ecdb105b85af5e1033668b9fc6e4
Ming Ying <yingming18@gmail.com>
2018-08-17 11:24:59 -0400

    Change pagination function and display

--- javascript/appModels.js
+++ javascript/appModels.js
@@ -59,11 +59,12 @@ appMixture.PredictionModel = Backbone.Model.extend({
 
 appMixture.PredictionResultModel = Backbone.Model.extend({
     defaults: {
+        pageSizeChoices: [ 10, 15, 20, 25, 50, 100, 150, 200, 250],
         pageNum: 1,
         pages: 0,
         start: 0,
-        end: 12,
-        pageSize: 12
+        end: 25,
+        pageSize: 25
     },
     parse: function(res) {
         res.pageNum = 1;
@@ -73,6 +74,7 @@ appMixture.PredictionResultModel = Backbone.Model.extend({
         res.start = 0;
         res.end = this.defaults.pageSize < res.results.prediction.length ? this.defaults.pageSize : res.results.prediction.length;
         res.pageSize = this.defaults.pageSize;
+        res.pageSizeChoices = this.defaults.pageSizeChoices;
         return res;
     },
     url: "predict"
--- javascript/appViews.js
+++ javascript/appViews.js
@@ -16,6 +16,7 @@ var appMixture = {
         'outcomeL',
         'outcomeR'
     ],
+    MAX_PAGES: 5,
     currentView: null,
     showView: function(view) {
         if (this.currentView !== null && this.currentView.cid !== view.cid) {
@@ -862,6 +863,7 @@ appMixture.PredictionResultView = Backbone.View.extend({
     id: 'results',
     className: 'col-md-8',
     events: {
+        'input #pageSize': 'changePageSize',
         'click .pageNav': 'changePage'
     },
     initialize: function() {
@@ -869,30 +871,67 @@ appMixture.PredictionResultView = Backbone.View.extend({
             'variable': 'data'
         });
         this.model.on({
-            'change:results': this.render
+            'change:results change:end change:start': this.render
         }, this);
-        this.model.on({
-            'change:end': this.render
-        }, this);
+    },
+    changePageSize: function(e) {
+        var pageSize = parseInt(e.target.value);
+        var resultLength = 0;
+        if (this.model.get('results') && this.model.get('results').prediction) {
+            resultLength = this.model.get('results').prediction.length;
+        }
+        var pages = Math.ceil(resultLength / pageSize);
+        var pageNum = Math.floor(this.model.get('start') / pageSize) + 1;
+        this.model.set('pageSize', pageSize, {silent: true});
+        this.model.set('pages', pages, {silent: true});
+        this.model.set('pageNum', pageNum, {silent: true});
+        this.calculatePageBoundaries();
     },
     changePage: function(e) {
         e.preventDefault();
         var pageNum = parseInt(e.target.dataset.pageNum);
         if (pageNum) {
-            var pageSize = this.model.get('pageSize');
-            var resultLength = 0;
-            if (this.model.get('results') && this.model.get('results').prediction) {
-                resultLength = this.model.get('results').prediction.length;
-            }
-            var start = (pageNum -1) * pageSize;
-            var end = pageNum * pageSize;
-            var end = end > resultLength ? resultLength : end;
             this.model.set('pageNum', pageNum, {silent: true});
-            this.model.set('start', start, {silent: true});
-            this.model.set('end', end);
+            this.calculatePageBoundaries();
         }
     },
+    calculatePageBoundaries: function() {
+        var pageNum = this.model.get('pageNum');
+        var pageSize = this.model.get('pageSize');
+        var resultLength = 0;
+        if (this.model.get('results') && this.model.get('results').prediction) {
+            resultLength = this.model.get('results').prediction.length;
+        }
+        var start = (pageNum -1) * pageSize;
+        var end = pageNum * pageSize;
+        var end = end > resultLength ? resultLength : end;
+        this.model.set('start', start, {silent: true});
+        this.model.set('end', end, {silent: true});
+        this.model.trigger('change:end', this.model);
+    },
+    calculateNeighborPages: function() {
+        var currentPage = this.model.get('pageNum');
+        var start = currentPage - Math.floor(appMixture.MAX_PAGES / 2);
+        if (start < 1) {
+            start = 1;
+        }
+        var end = start + appMixture.MAX_PAGES -1;
+        if (end > this.model.get('pages')) {
+            var emptySpace = end - this.model.get('pages');
+            end = this.model.get('pages');
+            start -= emptySpace;
+            if (start < 1) {
+                start = 1;
+            }
+        }
+        var pages = [];
+        for (var i = start; i <= end; ++i) {
+            pages.push(i);
+        }
+        this.model.set('neighborPages', pages, {silent: true});
+    },
     render: function() {
+        this.calculateNeighborPages();
         this.$el.html(this.template(this.model.attributes));
         return this;
     }
