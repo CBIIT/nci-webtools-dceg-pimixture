f0bef5f5f11cd54e66f71055cfd9c019534d0d09
Ming Ying <yingming18@gmail.com>
2018-10-11 14:04:04 -0400

    Refactor out some shared variables and functions.

--- pimixture.py
+++ pimixture.py
@@ -10,21 +10,7 @@ from s3 import S3
 
 app = Flask(__name__)
 
-INPUT_DATA_PATH = os.environ['PIMIXTURE_INPUT_DATA_FOLDER'] if 'PIMIXTURE_INPUT_DATA_FOLDER' in os.environ else 'tmp'
-print('INPUT_DATA_PATH: {}'.format(INPUT_DATA_PATH))
-if not os.path.isdir(INPUT_DATA_PATH):
-    os.makedirs(INPUT_DATA_PATH)
-
-OUTPUT_DATA_PATH = os.environ['PIMIXTURE_OUTPUT_DATA_FOLDER'] if 'PIMIXTURE_OUTPUT_DATA_FOLDER' in os.environ else 'tmp'
-print('OUTPUT_DATA_PATH: {}'.format(OUTPUT_DATA_PATH))
-if not os.path.isdir(OUTPUT_DATA_PATH):
-    os.makedirs(OUTPUT_DATA_PATH)
-# TEMP_PATH = os.environ['PIMIXTURE_DATA_FOLDER'] if 'PIMIXTURE_DATA_FOLDER' in os.environ else 'tmp'
-
-INPUT_FILE_PREFIX = 'pimixtureInput_'
-OUTPUT_FILE_PREFIX = 'pimixtureOutput_'
-
-IMPORT_R_WRAPPER = 'source("R/pimixtureWrapper.R")'
+from util import *
 
 def buildFailure(message,statusCode = 500):
     response = jsonify(message)
@@ -438,18 +424,6 @@ def uploadModelFile():
         else:
             print(rOutput)
             return buildFailure(rOutput, 500)
-
-
-def getInputFilePath(id, extention):
-    return getFilePath(INPUT_DATA_PATH, INPUT_FILE_PREFIX, id, extention)
-
-
-def getOutputFilePath(id, extention):
-    return getFilePath(OUTPUT_DATA_PATH, OUTPUT_FILE_PREFIX, id, extention)
-
-def getFilePath(path, prefix, id, extention):
-    filename = prefix + id + extention
-    return os.path.join(path, filename)
 
 if __name__ == '__main__':
     import argparse
---
+++ util.py
@@ -0,0 +1,28 @@
+import os
+
+INPUT_DATA_PATH = os.environ['PIMIXTURE_INPUT_DATA_FOLDER'] if 'PIMIXTURE_INPUT_DATA_FOLDER' in os.environ else 'tmp'
+print('INPUT_DATA_PATH: {}'.format(INPUT_DATA_PATH))
+if not os.path.isdir(INPUT_DATA_PATH):
+    os.makedirs(INPUT_DATA_PATH)
+
+OUTPUT_DATA_PATH = os.environ['PIMIXTURE_OUTPUT_DATA_FOLDER'] if 'PIMIXTURE_OUTPUT_DATA_FOLDER' in os.environ else 'tmp'
+print('OUTPUT_DATA_PATH: {}'.format(OUTPUT_DATA_PATH))
+if not os.path.isdir(OUTPUT_DATA_PATH):
+    os.makedirs(OUTPUT_DATA_PATH)
+# TEMP_PATH = os.environ['PIMIXTURE_DATA_FOLDER'] if 'PIMIXTURE_DATA_FOLDER' in os.environ else 'tmp'
+
+INPUT_FILE_PREFIX = 'pimixtureInput_'
+OUTPUT_FILE_PREFIX = 'pimixtureOutput_'
+
+IMPORT_R_WRAPPER = 'source("R/pimixtureWrapper.R")'
+
+def getInputFilePath(id, extention):
+    return getFilePath(INPUT_DATA_PATH, INPUT_FILE_PREFIX, id, extention)
+
+def getOutputFilePath(id, extention):
+    return getFilePath(OUTPUT_DATA_PATH, OUTPUT_FILE_PREFIX, id, extention)
+
+def getFilePath(path, prefix, id, extention):
+    filename = prefix + id + extention
+    return os.path.join(path, filename)
+
