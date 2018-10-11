7f7ebada5f5b0a288cb2ccc8fb54475369c488ce
Ming Ying <yingming18@gmail.com>
2018-10-11 14:29:21 -0400

    Capture Ctrl + C signal to exit receiver peacefully.

--- receiver.py
+++ receiver.py
@@ -21,51 +21,52 @@ def sendErrors(errors):
     return True
 
 if __name__ == '__main__':
-    sqs = SQS()
-    while True:
-        print("Receiving more messages...")
-        for msg in sqs.receiveMsgs():
-            try:
-                data = json.loads(msg.body)
-                if data and 'jobType' in data and data['jobType'] == 'fitting':
-                    print('Got a job!')
+    try:
+        sqs = SQS()
+        while True:
+            print("Receiving more messages...")
+            for msg in sqs.receiveMsgs():
+                try:
+                    data = json.loads(msg.body)
+                    if data and 'jobType' in data and data['jobType'] == 'fitting':
+                        print('Got a job!')
 
-                    parameters = data['parameters']
-                    id = data['jobId']
-                    ext = data['extension']
-                    bucket = parameters['inputCSVFile']['bucket']
-                    key = parameters['inputCSVFile']['key']
+                        parameters = data['parameters']
+                        id = data['jobId']
+                        ext = data['extension']
+                        bucket = parameters['inputCSVFile']['bucket']
+                        key = parameters['inputCSVFile']['key']
 
-                    downloadFileName = getInputFilePath(id, ext)
-                    print('Download from {} to {}'.format(bucket, downloadFileName))
-                    s3 = S3(bucket=bucket)
-                    s3.downloadFile(key, downloadFileName)
-                    parameters['filename'] = downloadFileName
+                        downloadFileName = getInputFilePath(id, ext)
+                        print('Download from {} to {}'.format(bucket, downloadFileName))
+                        s3 = S3(bucket=bucket)
+                        s3.downloadFile(key, downloadFileName)
+                        parameters['filename'] = downloadFileName
 
-                    outputRdsFileName = getOutputFilePath(id, '.rds')
-                    outputCSVFileName = getOutputFilePath(id, '.csv')
-                    outputFileName = getOutputFilePath(id, '.out')
-                    parameters['outputRdsFilename'] = outputRdsFileName
-                    parameters['outputFilename'] = outputFileName
+                        outputRdsFileName = getOutputFilePath(id, '.rds')
+                        outputCSVFileName = getOutputFilePath(id, '.csv')
+                        outputFileName = getOutputFilePath(id, '.out')
+                        parameters['outputRdsFilename'] = outputRdsFileName
+                        parameters['outputFilename'] = outputFileName
 
-                    fittingResult = fitting(parameters, outputCSVFileName)
-                    if fittingResult['status']:
-                        if not sendResults(fittingResult['results']):
-                            print("An error happened when trying to send result email")
+                        fittingResult = fitting(parameters, outputCSVFileName)
+                        if fittingResult['status']:
+                            if not sendResults(fittingResult['results']):
+                                print("An error happened when trying to send result email")
+                        else:
+                            if not sendErrors(fittingResult):
+                                print("An error happened when trying to send error email")
+
+                        s3.deleteFile(key)
+                        msg.delete()
                     else:
-                        if not sendErrors(fittingResult):
-                            print("An error happened when trying to send error email")
+                        pprint(data)
+                        print('Unknown message type!')
+                except Exception as e:
+                    print(e)
 
-                    s3.deleteFile(key)
-                    msg.delete()
-                else:
-                    pprint(data)
-                    print('Unknown message type!')
-            except KeyboardInterrupt:
-                    print("Bye")
-                    sys.exit()
-            except Exception as e:
-                print(e)
-
-            finally:
-                pass
+                finally:
+                    pass
+    except KeyboardInterrupt:
+        print("\nBye!")
+        sys.exit()
