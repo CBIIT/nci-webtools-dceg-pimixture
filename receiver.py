c8f1810226785916d6102281ebe91838c31fff9a
Ming Ying <yingming18@gmail.com>
2018-10-12 11:13:33 -0400

    Remove output files after uploaded them to S3.

--- receiver.py
+++ receiver.py
@@ -37,13 +37,13 @@ if __name__ == '__main__':
                         parameters = data['parameters']
                         id = data['jobId']
                         ext = data['extension']
-                        bucket = parameters['inputCSVFile']['bucket']
-                        key = parameters['inputCSVFile']['key']
+                        inputBucket = parameters['inputCSVFile']['bucket']
+                        inputFileName = parameters['inputCSVFile']['key']
 
                         downloadFileName = getInputFilePath(id, ext)
-                        print('Download from {} to {}'.format(bucket, downloadFileName))
-                        inputBucket = S3Bucket(bucket)
-                        inputBucket.downloadFile(key, downloadFileName)
+                        print('Download from {} to {}'.format(inputBucket , downloadFileName))
+                        inputBucket = S3Bucket(inputBucket)
+                        inputBucket.downloadFile(inputFileName, downloadFileName)
                         parameters['filename'] = downloadFileName
 
                         outputRdsFileName = getOutputFilePath(id, '.rds')
@@ -55,31 +55,37 @@ if __name__ == '__main__':
                         fittingResult = fitting(parameters, outputCSVFileName)
                         if fittingResult['status']:
                             outputBucket = S3Bucket(OUTPUT_BUCKET)
-                            object = outputBucket.uploadFile(getOutputFileName(id, '.rds'), outputRdsFileName)
+                            outputRdsFileKey = getOutputFileName(id, '.rds')
+                            object = outputBucket.uploadFile(outputRdsFileKey, outputRdsFileName)
+                            os.remove(outputRdsFileName)
                             if object:
                                 fittingResult['results']['Rfile'] = object
-                                os.remove(outputRdsFileName)
                             else:
-                                sys.exit(1)
+                                sendErrors('Upload result RDS file failed!')
+                                continue
 
                             object = outputBucket.uploadFile(getOutputFileName(id, '.csv'), outputCSVFileName)
+                            os.remove(outputCSVFileName)
                             if object:
                                 fittingResult['results']['csvFile'] = object
-                                os.remove(outputCSVFileName)
                             else:
-                                sys.exit(1)
+                                sendErrors('Upload result CSV file failed!')
+                                outputBucket.deleteFile(outputRdsFileKey)
+                                continue
 
                             if not sendResults(fittingResult['results']):
                                 print("An error happened when trying to send result email")
+                                continue
                         else:
                             if not sendErrors(fittingResult):
                                 print("An error happened when trying to send error email")
 
-                        inputBucket.deleteFile(key)
+                        inputBucket.deleteFile(inputFileName)
                         msg.delete()
                     else:
                         pprint(data)
                         print('Unknown message type!')
+                        msg.delete()
                 except Exception as e:
                     print(e)
 
