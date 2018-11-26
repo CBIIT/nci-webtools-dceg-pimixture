3606726b37c2f2a1a66e379ae4513c51c93941c8
Ming Ying <yingming18@gmail.com>
2018-11-26 09:50:17 -0500

    Move TestData.Check.R to R folder.

--- R/TestData.Check.R
+++ R/TestData.Check.R
@@ -1,7 +1,7 @@
 testdata.check<-function(fit,test.data,data.type){
     fml<-as.formula(fit$p.model)
     var.list<-all.vars(fml)
-    VALUE<-identical(names(test.data),var.list[-seq(1,3)])
+    VALUE<-identical(sort(names(test.data)),sort(var.list[-seq(1,3)]))
     if(VALUE==TRUE){
       factor.var<-data.type$text[data.type$type=="nominal"]
       factor.ind<-which(names(test.data) %in% factor.var)
@@ -35,40 +35,16 @@ testdata.check<-function(fit,test.data,data.type){
 }
 #If VALUE=TRUE, test.data is valid for prediction; otherwise, it is not valid for prediction.
 
-########## Relabel function for prediction output #########################
 predict.relabel<-function(test.data,time.points){
-
-
-  collapse<-function(x){
-    a<-x[1]
-    for(i in 2:length(x)){
-      a<-paste0(a,",",x[i])
-    }
-    return(a)
-  }
-
-
-  var.names<-names(test.data)
-
+  
   if(nrow(test.data)>1){
-    if(length(var.names)==1){
-      relabel<-paste0(var.names,"=",test.data[,1])
-    }else {
-      relabel2<-t(apply(test.data,1, function(x) paste0(var.names,"=",x)))
-      relabel<-apply(relabel2,1,collapse )
-    }
-
+    var.names<-colnames(test.data)
+    relabel<-t(apply(test.data,1, function(x) paste0(var.names,"=",x)))
   }else if(nrow(test.data)==1){
-    if(length(var.names)==1){
-      relabel<-paste0(var.names,"=",test.data)
-    }else {
-      relabel2<-relabel<-paste0(var.names,"=",test.data)
-      relabel<-collapse(relabel2)
-    }
+    var.names<-names(test.data)
+    relabel<-paste0(var.names,"=",test.data)
   }
-
-  output<-rep(relabel,each=length(time.points))
-
+  
+  output<-as.data.frame(lapply(data.frame(relabel), rep, length(time.points)))
   return(output)
 }
-
