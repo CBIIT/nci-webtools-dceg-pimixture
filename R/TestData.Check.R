9074c287221aee56b2f3538bbdd7067f65306f92
Ming Ying <yingming18@gmail.com>
2018-11-26 09:53:52 -0500

    Updated testdata.check R function.

--- R/TestData.Check.R
+++ R/TestData.Check.R
@@ -2,33 +2,23 @@ testdata.check<-function(fit,test.data,data.type){
     fml<-as.formula(fit$p.model)
     var.list<-all.vars(fml)
     VALUE<-identical(sort(names(test.data)),sort(var.list[-seq(1,3)]))
+    
     if(VALUE==TRUE){
       factor.var<-data.type$text[data.type$type=="nominal"]
       factor.ind<-which(names(test.data) %in% factor.var)
-      factor.cat<-data.type$category[data.type$type=="nominal"]
-      
+     
+      # factor.cat<-data.type$category[data.type$type=="nominal"]
       n.samp<-nrow(test.data)
       
       if(length(factor.ind)>0){
+        
         for(i in factor.ind){
           test.data[,i]<-as.factor(test.data[,i])
+          ref.ind<-which(data.type$text==names(test.data)[i])
+          test.data[,i]<-relevel(test.data[,i],ref=data.type$cat[ref.ind])
         }
       }
-      
-      #######Set reference group ######################
-      relevel.function<-function(x,y){
-        xx<-factor.ind[x]
-        ref.level<-which(levels(test.data[,xx]) %in% as.character(y))
-        test.data[,xx] <- relevel(test.data[,xx], ref = ref.level)
-        return(test.data)
-      }
-      #########################################
-      if(length(factor.ind)>0){
-        for(i in 1:length(factor.ind)){
-          test.data<-relevel.function(i,factor.cat[i])
-        }
-      }
-      return(test.data)
+            return(test.data)
     }else{
       stop("The variable names of the test dataset do not match with the model")
     }
